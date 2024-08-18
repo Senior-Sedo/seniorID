c377d93487811c9a6698b6e00425b78055016887
Senior-Sidou <sidahmed01234567@gmail.com>
2024-08-18 01:14:50 +0100

    made it better

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,26 @@
-const l = (c: string, n: number): string => {
-  let s: string = "";
-  for (let i = 0; i < n; i++) {
-    s += c.charAt(Math.floor(Math.random() * ((c.length - 1) + 1)));
+const used = new Set<string>();
+
+const loop = (characters: string, num: number): string => {
+  let str: string = "";
+  for (let i = 0; i < num; i++) {
+    str += characters.charAt(Math.floor(Math.random() * ((characters.length - 1) + 1)));
   }
-  return s;
+  return str;
 }
 const seniorID = (lengthArray: number[] = [8, 4, 4, 4, 12], characters: string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"): string => {
-  let d: string = "";
-  lengthArray.forEach((c, i) => d += `${l(characters, c)}${i === lengthArray.length - 1 ? "" : "-"}`)
-  return d;
+  characters = [...new Set(characters)].join("");
+  let id: string, run: number = 0;
+
+  do {
+    id = "";
+    lengthArray.forEach((num, index) => id += `${loop(characters, num)}${index === lengthArray.length - 1 ? "" : "-"}`);
+    run++;
+    if (run >= Math.pow(id.length, id.length)) lengthArray[lengthArray.length - 1]!++;
+  } while(used.has(id));
+
+  used.add(id);
+  
+  return id;
 };
 
 export default seniorID;
